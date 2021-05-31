6b1dad52809d6a2994988645813a83333a8e0450
walotta <walotta@126.com>
2021-06-01 01:39:24 +0800

    add new function

--- front/js/function_pool.js
+++ front/js/function_pool.js
@@ -17,4 +17,27 @@ function setCookie(cname,cvalue,exdays)
     var expires = "expires="+d.toGMTString();
     document.cookie = cname + "=" + cvalue + "; " + expires;
 }
-        
+
+function query_profile(userId)
+{
+    //not use
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.open("POST","/php/query_profile.php",true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send("userId="+userId);
+    while(xmlhttp.readyState==4&&xmlhttp.status==200)return xmlhttp.responseText;
+}
+
+function randomNum(minNum,maxNum){ 
+    switch(arguments.length){ 
+        case 1: 
+            return parseInt(Math.random()*minNum+1,10); 
+        break; 
+        case 2: 
+            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+        break; 
+            default: 
+                return 0; 
+            break; 
+    } 
+} 
