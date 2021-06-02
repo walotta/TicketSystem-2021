76738c9fa0f9f59897a8ad4dd30548ddc6ae4da5
walotta <walotta@126.com>
2021-06-02 16:58:02 +0800

    finish without add train

--- front/php/add_user.php
+++ front/php/add_user.php
@@ -1,24 +1,16 @@
 <?php
-    $curId=$_POST['curId'];
     $userId=$_POST['userId'];
     $userName=$_POST['userName'];
     $mail=$_POST['mail'];
     $psw=$_POST['psw'];
     $pri=$_POST['pri'];
 
-    if($userId==""||$userName==""||$mail==""||$pri=="")
+    if($userId==""||$userName==""||$mail==""||$pri==""||$psw=="")
     {
         echo "invaild";
     }else
     {
-        $cmd="";
-        if($psw=="")
-        {
-            $cmd="modify_profile -c walotta_admin -u $userId -n $userName -m $mail -g $pri";
-        }else
-        {
-            $cmd="modify_profile -c walotta_admin -u $userId -p $psw -n $userName -m $mail -g $pri";
-        }
+        $cmd="add_user -c walotta_admin -u $userId -p $psw -n $userName -m $mail -g $pri";
         $socket = socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
         socket_connect($socket,'ticket.walotta.top',9988);
         socket_write($socket,strlen($cmd).$cmd);
